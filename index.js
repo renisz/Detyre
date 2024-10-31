1293bb25b3abd12c06d969ed6d8d7de83f7c6971
renisz <renisziri07@gmail.com>
2024-10-31 11:17:35 -1000

    Add files via upload

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
 document.getElementById('userForm').addEventListener('submit', function(event) {
     event.preventDefault(); 
 
-    const email = document.getElementById('email').value;
-    const phone = document.getElementById('phone').value;
-    const password = document.getElementById('password').value;
+    const email = document.getElementById('email').value
+    const phone = document.getElementById('phone').value
+    const password = document.getElementById('password').value
 
-    const emailMessage = document.getElementById('emailMessage');
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailPattern.test(email)) {
-        emailMessage.textContent = 'Emaili nuk është në formatin e duhur.';
-        console.log('Emaili nuk është në formatin e duhur.');
+    const emailMessage = document.getElementById('emailMessage')
+
+    const emailTemplate = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    if (!emailTemplate.test(email)) {
+        emailMessage.textContent = 'Emaili nuk eshte ne formatin e duhur'
     } else {
-        emailMessage.textContent = '';
-        console.log('Emaili është valid.');
+        
+        emailMessage.textContent = 'ok'
     }
 
-    const phoneMessage = document.getElementById('phoneMessage');
-    const phonePattern = /^(068|069)\d{7}$/;
-    if (!phonePattern.test(phone)) {
-        phoneMessage.textContent = 'Numri i telefonit duhet të fillojë me 068 ose 069 dhe të ketë 10 numra.';
-        console.log('Numri i telefonit është i pavlefshëm.');
+    const phoneMessage = document.getElementById('phoneMessage')
+    const telTemplate = /^(068|069)\d{7}$/
+    if (!telTemplate.test(phone)) {
+        phoneMessage.textContent = 'Numri i telefonit duhet te filloje me 068 ose 069 dhe te kete 10 numra'
     } else {
-        phoneMessage.textContent = '';
-        console.log('Numri i telefonit është valid.');
+
+        phoneMessage.textContent = 'ok'
     }
-    const passwordMessage = document.getElementById('passwordMessage');
-    const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
-    if (!passwordPattern.test(password)) {
-        passwordMessage.textContent = 'Fjalëkalimi duhet të ketë të paktën 8 karaktere, një germe të madhe, një germe të vogël dhe një numër.';
-        console.log('Fjalëkalimi është i pavlefshëm.');
-    } else {
-        passwordMessage.textContent = '';
-        console.log('Fjalëkalimi është valid.');
+    const passwordMessage = document.getElementById('passwordMessage')
+    const passTemplate = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/;
+    if (!passTemplate.test(password)) {
+        passwordMessage.textContent = 'Fjalekalimi duhet te kete te pakten 8 karaktere, nje germe te madhe,nje germe te vogel dhe nje numer.';
+    } 
+    else {
+        passwordMessage.textContent = 'ok'
     }
-});
+}
+)
